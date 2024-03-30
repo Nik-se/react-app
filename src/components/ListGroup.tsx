a5b9eab530cc3cbb1f1d9cd378ffe0d496969448
Nik-se <nikola.se.markovic@gmail.com>
2024-03-30 15:12:00 -0600

    message

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,17 +2,12 @@ function ListGroup() {
   let items = ["New York", "Chicago", "Denver", "Phoenix", "Los Angeles"];
   items = [];
 
-  if (items.length === 0)
-    return (
-      <>
-        <h1>List</h1>
-        <p>No Item found</p>
-      </>
-    );
+  const message = items.length === 0 ? <p>No Item found</p> : null;
 
   return (
     <>
       <h1>LIST</h1>
+      {message}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
