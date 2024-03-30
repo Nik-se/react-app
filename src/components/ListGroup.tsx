0225c4f56b9915b61800ba9848618e8737f9cc22
Nik-se <nikola.se.markovic@gmail.com>
2024-03-30 15:46:04 -0600

    added markup

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,18 +1,30 @@
+import { useState } from "react";
+
 function ListGroup() {
   let items = ["New York", "Chicago", "Denver", "Phoenix", "Los Angeles"];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   const message = items.length === 0 ? <p>No Item found</p> : null;
+
+  // const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
       <h1>LIST</h1>
       {message}
       <ul className="list-group">
-        {items.map((item) => (
+        {items.map((item, index) => (
           <li
-            className="list-group-item"
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
             key={item}
-            onClick={(event) => console.log(item, event)}
+            onClick={(event) => {
+              console.log(item, index, event);
+              setSelectedIndex(index);
+            }}
           >
             {item}
           </li>
