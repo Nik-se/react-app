7d8ec20dc1de665aebf1ed94304e2606af73df25
Nik-se <nikola.se.markovic@gmail.com>
2024-03-31 13:53:05 -0600

    added score

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,46 @@ function App() {
   const [stateData, setStateData] = useState([]);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [answers, setAnswers] = useState<Answer[]>([]);
+  const [started, setStarted] = useState(false); // State to track if the "Start" button is clicked
+  const [submitted, setSubmitted] = useState(false);
+  const [score, setScore] = useState(0);
 
-  useEffect(() => {
-    async function fetchStateData() {
-      const data = await State();
-      setStateData(data);
+  const handleStart = async () => {
+    try {
+      const data = await State(); // Fetch state data
+      setStateData(data); // Set state data
+      setStarted(true); // Set started to true after clicking "Start"
+      setSubmitted(false);
+    } catch (error) {
+      console.error("Error fetching state data:", error);
     }
-    fetchStateData();
-  }, []);
+  };
+
+  // Conditionally render the section only if started is true
+  if (!started || submitted) {
+    return (
+      <>
+        <div>
+          <h1 className="text-center text-bg-primary p-3">
+            USA STATES CAPITOLS
+          </h1>
+        </div>
+        <div className="d-flex justify-content-center p-3">
+          <div>
+            <Card />
+          </div>
+        </div>
+        {submitted && (
+          <div className="text-center">
+            <strong>Your Score : {score}/100</strong>
+          </div>
+        )}
+        <div className="text-center">
+          <Button onClick={handleStart} label="Start" />
+        </div>
+      </>
+    );
+  }
 
   if (!stateData.length) {
     // Render loading state while fetching data
@@ -51,7 +83,10 @@ function App() {
 
   const handleSubmit = async () => {
     try {
-      await SubmitAnswers(answers);
+      const response = await SubmitAnswers(answers);
+      const points = response.data.points;
+      setScore(points);
+      setSubmitted(true);
       console.log("Answers submitted successfully!");
     } catch (error) {
       console.error("Error submitting answers:", error);
@@ -80,11 +115,6 @@ function App() {
           <div>
             <Button onClick={handleSubmit} label="Submit" />
           </div>
-        </div>
-      </div>
-      <div className="d-flex justify-content-center p-5">
-        <div>
-          <Card />
         </div>
       </div>
     </>
